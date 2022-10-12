c1f7717a1410d698353659058a99f2449cbe1a7f
Henrik Andersen <han@rts.dk>
2022-10-12 11:02:01 +0200

    add ui elements

--- js/minesweeper.js
+++ js/minesweeper.js
@@ -1,12 +1,38 @@
 import MineBoard from "./mine-board.js";
 
+let uiContainer = document.createElement('div');
 let resetButton = document.createElement('button');
+let mineCountLabel= document.createElement('label');
+let mineCountInput = document.createElement('input');
+let boardSizeLabel = document.createElement('label');
+let boardSizeInput = document.createElement('input');
 let board = new MineBoard();
+
+resetButton.textContent = 'Reset';
+mineCountLabel.textContent = '💣';
+mineCountInput.type = 'number';
+mineCountInput.value = board.minesRemaining;
+
+boardSizeLabel.textContent = '📏';
+boardSizeInput.type = 'number';
+boardSizeInput.value = board.width;
+
+uiContainer.classList.add('mine-ui-container');
+
+document.body.appendChild(board.element);
+document.body.appendChild(uiContainer);
+uiContainer.appendChild(resetButton);
+uiContainer.appendChild(mineCountLabel);
+uiContainer.appendChild(boardSizeLabel);
+mineCountLabel.appendChild(mineCountInput);
+boardSizeLabel.appendChild(boardSizeInput);
 
 resetButton.addEventListener('click', () => board.reset());
 
-resetButton.textContent = 'Reset';
-resetButton.classList.add('mine-button');
+[mineCountLabel, boardSizeInput].forEach(input => input.addEventListener('change', () => {
+    let oldBoardElement = board.element;
+    board = new MineBoard(+boardSizeInput.value, +boardSizeInput.value, mineCountInput.value);
+    document.body.replaceChild(board.element, oldBoardElement);
 
-document.body.appendChild(board.element);
-document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', () => board.reset());
+}));
